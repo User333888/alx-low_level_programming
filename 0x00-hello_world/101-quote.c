723db4e5111e6a7854c7459f3fa9ca1d83a4ceed
User333888 <kiara.g@hotmail.co.za>
2023-02-19 03:57:15 -0800

    final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- prints exactly
+ * and that piece of art is useful" 
+ * - Dora Korpar, 2015-10-19
+ *
+ * Return: Always (1) standard error
+ *
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
