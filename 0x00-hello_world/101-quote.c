867f3ff87fc1f70fbe0b3376d8ce19b7dbf5b811
User333888 <kiara.g@hotmail.co.za>
2023-02-19 05:10:32 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,11 @@
 #include <unistd.h>
 
 /**
- * main- prints exactly
- * and that piece of art is useful\" 
- * - Dora Korpar, 2015-10-19
- * Return: Always (1) standard error
+ * main - prints a string to standard error
+ * Return: Program returns 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
