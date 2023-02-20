8774240c2eb2fdc6fc0e6b0f4ac2a7b1f8542d0e
User333888 <kiara.g@hotmail.co.za>
2023-02-20 12:29:07 -0800

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- prints singles digit numbers
+ * of base 10 from 0-9
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{	putchar(num);
+	}
+	putchar('\n');
+	return(0);
+}
