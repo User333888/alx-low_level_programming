6d1d7d882f217cf0ca73e1a1e70a9c5b4d11650c
User333888 <kiara.g@hotmail.co.za>
2023-02-20 10:36:08 -0800

    i think it will work now

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,15 @@
  *  main- uses putchar to print
  *  an array of characters
  *  Return: Always 0 (SUCCESS)
- *
  */
 int main(void)
 {
 	char az;
 
-	for (az = 'a'; az <= 'z'; az++);
+	for (az = 'a'; az <= 'z'; az++)
+	{
 		putchar(az);
+	}
 	putchar('\n');
 	return (0);
 }
