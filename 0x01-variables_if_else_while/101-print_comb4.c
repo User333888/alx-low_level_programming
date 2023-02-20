96715b8bf181b881e61477d2634ee3b154e23885
User333888 <kiara.g@hotmail.co.za>
2023-02-20 13:58:41 -0800

    getting the hang of it

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints smallest possible
+ * combinations of three digit numbers
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if (a != b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a == '7' && b == '8' && c == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
+
