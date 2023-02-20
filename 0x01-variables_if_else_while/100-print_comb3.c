0a8eaaa40c719e4510b5bc408175bc911f06a533
User333888 <kiara.g@hotmail.co.za>
2023-02-20 13:35:44 -0800

    save your changes

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* print all combinations of two digits
+ * with specifications
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int i;
+	int f;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (f = i +1; f <= '9'; f++)
+		{
+			if (i != f)
+			{
+				putchar(i);
+				putchar(f);
+
+				if (i == '8' && f == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
