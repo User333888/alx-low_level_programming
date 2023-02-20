c9338b36e16356351ff1cf1d7835081305a400bf
User333888 <kiara.g@hotmail.co.za>
2023-02-20 13:15:17 -0800

    save your changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
-	char num;
+	int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{	putchar(num);
+		if (num != '9')
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
