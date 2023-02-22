d4a71032cea2816fcedfdcc8b8b6dc3364261e6a
User333888 <kiara.g@hotmail.co.za>
2023-02-21 19:45:34 -0800

    task 4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,14 @@
  * @n: is the integer argument being passed
  * Return: Always 0 (SUCCESS)
  */
+void charline(int);
 int print_last_digit(int n)
 {
 	int a;
 
 	if (n < 0)
 		n = -n;
-	
+
 	a = n % 10;
 
 	if (a < 0)
