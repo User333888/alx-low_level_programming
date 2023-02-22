9333a70906ca3b65b0fc51f92dbc19d0972823aa
User333888 <kiara.g@hotmail.co.za>
2023-02-21 19:17:15 -0800

    task 4

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * of an integer
+ * @n; integer being tested
+ * Return: void.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+		return (n);
+}
