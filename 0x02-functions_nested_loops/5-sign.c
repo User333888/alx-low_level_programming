9aea3dfe6ce55533763b989f4c58291e5f4ac83f
User333888 <kiara.g@hotmail.co.za>
2023-02-21 18:45:39 -0800

    task 4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,19 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf("-");
 		return (-1);
+		printf("-");
 	}
 	else if (n > 0)
 	{
-		printf("+");
 		return (1);
+		printf("+");
 	}
-	else (n = 0);
+	else (n == 0);
 	{
-		printf("0");
 		return (0);
+		printf("0");
 	}
+	return (0);
 }
 
