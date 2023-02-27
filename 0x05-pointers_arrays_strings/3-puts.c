5113ee3fd1341bbf56a9d844639b7cecd93373ef
User333888 <kiara.g@hotmail.co.za>
2023-02-27 15:38:40 -0800

    o

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string value
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i > 0; i++)
+	{
+		if (str[i] != '\0')
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
+}
