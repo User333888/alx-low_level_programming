a1d8ef93f1c8d9db67ab121fe45d29c27c68ea1d
User333888 <kiara.g@hotmail.co.za>
2023-03-04 14:47:22 -0800

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,18 @@
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i > 0; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
-
-	while (n <= 0)
+	else
 	{
+		int i;
+
+		for (i > 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
