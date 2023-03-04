7a9046dfbee68dcb072df260fbcb601b603c0e9c
User333888 <kiara.g@hotmail.co.za>
2023-03-04 15:33:41 -0800

    line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square to standard output
+ * @n: the number of times to print \
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int b;
+
+	for (i = 0; i < size; i++)
+	{
+		for (b = 0; b < i; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
