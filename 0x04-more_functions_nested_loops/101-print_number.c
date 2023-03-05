3281790b455a1ba9421f56521886368cf8b4e644
User333888 <kiara.g@hotmail.co.za>
2023-03-04 17:14:08 -0800

    3:00am

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	_putchar(n);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,5 +13,7 @@ void print_diagonal(int n);
 void print_square(int size);
 int main(void);
 void print_triangle(int size);
+void prime_factor(int n);
+void print_number(int n);
 
 #endif
