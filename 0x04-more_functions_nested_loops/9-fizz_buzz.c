c268e4ceeb8dfa1db73b2475889efa4d9ed7ed18
User333888 <kiara.g@hotmail.co.za>
2023-03-04 16:11:40 -0800

    line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * main - print numbers 1 - 100, print fizz if the number is divisible by 3, print buzz if number is
+ * main - print numbers 1 - 100, print fizz if the number
+ * is divisible by 3, print buzz if number is
  * divisible by 5 and fizzbuzz if number is divisible by both 3 and 5
  * Return: 0
  */
