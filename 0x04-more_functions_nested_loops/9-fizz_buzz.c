152a38872a44cef69d4906b07500d45f07fabd1d
User333888 <kiara.g@hotmail.co.za>
2023-03-04 16:04:25 -0800

    line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,19 @@ int fizzbuzz(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+
+		else if ((i % 5) == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
+
+		else if ((i % 15) == 0)
 			printf("FizzBuzz");
+
 		else
 		{
 			printf("%d ", i);
 		}
-	}
+	} printf("\n");
 	return (0);
 }
