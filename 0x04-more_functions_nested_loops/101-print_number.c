ef04af681fc88f731bdfa16386726547ac2aa88a
User333888 <kiara.g@hotmail.co.za>
2023-03-04 17:28:05 -0800

    almost done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,6 +8,22 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
-	_putchar('\n');
+	unsigned int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar((i % 10) + '0');
 }
