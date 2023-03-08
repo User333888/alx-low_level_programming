fa371392dc2bf41491ae00fd987ae340b7aaf9a3
User333888 <kiara.g@hotmail.co.za>
2023-03-08 02:08:00 -0800

    pray for me

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints the x9 table starting from 0
+ * Return: void
+ */
+void times_table(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (j == 9)
+			{
+				printf("%d", i * j);
+			}
+			else
+			{
+			printf("%d,  ", i * j);
+			}
+			j++;
+		}
+	printf("\n");
+	i++;
+	}
+}
