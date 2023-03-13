61e0ece52f9a8a08eb70512bfd322e7819741880
User333888 <kiara.g@hotmail.co.za>
2023-03-13 15:35:18 -0700

    right on the first try

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ int i, j, k;
 
 if (n > 0 && n < 15)
 {
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			k = j * i;
 			if (j == 0)
