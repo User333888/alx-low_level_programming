0cc70502f75f3810cf0a56310053ebc58ce65773
User333888 <kiara.g@hotmail.co.za>
2023-03-13 13:52:39 -0700

    late night working

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,25 @@
  */
 void print_to_98(int n)
 {
-	for (n <= 0; n <= 98; n++)
+	for (n = 0; n <= 98; n++)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			break;
+		}
+		printf("%d, ", n);
+	}
+	for (n < 0; n <= 98; n++)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			break;
+		}
+		printf("%d, ", n);
+	}
+	for (n > 98; n >= 98; n--)
 	{
 		if (n == 98)
 		{
