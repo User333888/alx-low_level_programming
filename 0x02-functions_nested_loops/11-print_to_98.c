e5a0fb6bfa568065abde0157e79941c0d6a549ff
User333888 <kiara.g@hotmail.co.za>
2023-03-13 09:36:49 -0700

    push to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,49 +10,12 @@
  */
 void print_to_98(int n)
 {
-	int c;
-
-	if (n < 0)
+	for (n = 0; n <= 98; c++)
 	{
-		for (c = n; c <= 98; c++)
+		if (n == 98)
 		{
-			if ((c = n))
-			{
-				printf("%d", n);
-			}
-			else 
-			{
-				printf(", %d", n);
-			}
+			printf("%d", n);
 		}
+		printf("%d, ", n);
 	}
-	else if (n >= 0 && n <= 98)
-	{
-		for (c = n; c <= 98; c++)
-		{
-			if ((c = n))
-			{
-				printf("%d", n);
-			}
-			else
-			{
-				printf(", %d", n);
-			}
-		}
-	}
-	else if (n > 98)
-	{
-		for (c = n; c >=98; c--)
-		{
-			if ((c = n ))
-			{
-				printf("%d", n);
-			}
-			else
-			{
-				printf(", %d", n);
-			}
-		}
-	}
-	printf("\n");
 }
